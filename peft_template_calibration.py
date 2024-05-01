8ca0cc4f4369f1b92a3f4dfdc14cbcef640cce11
Mrrrat <61361613+Mrrrat@users.noreply.github.com>
2024-05-01 15:40:24 +0300

    Update peft_template_calibration.py

--- peft_template_calibration.py
+++ peft_template_calibration.py
@@ -140,6 +140,8 @@ if __name__ == "__main__":
             print(f"Default Std: {config['Default Std']}")
             config['Default Mean Accuracy'] = (np.array(labels)[test_template_probs.argmax(2)] == np.array(test['target'])).mean(1).mean()
             print(f"Default Mean Accuracy: {config['Default Mean Accuracy']}")
+            config['Default Ensemble Accuracy'] = (np.array(labels)[train_template_probs.argmax(2)] == np.array(train['target'])).mean(1).mean()
+            print(f"DDefault Ensemble Accuracy: {config['Default Ensemble Accuracy']}")
 
             train_ensemble = train_template_probs.mean(dim=0)
             test_ensemble = test_template_probs.mean(dim=0)
